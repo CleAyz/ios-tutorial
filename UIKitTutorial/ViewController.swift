ac2ce0189dac87f28a64fedfbe78b3dc5bdfeaa2
CleAyz <clementayz@gmail.com>
2025-11-20 17:31:25 +0800

    Initial Commit

---
+++ UIKitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTutorial
+//
+//  Created by Clement Ang on 20/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIKitTutorialTests/UIKitTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitTutorialTests.swift
+//  UIKitTutorialTests
+//
+//  Created by Clement Ang on 20/11/25.
+//
+
+import Testing
+@testable import UIKitTutorial
+
+struct UIKitTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
