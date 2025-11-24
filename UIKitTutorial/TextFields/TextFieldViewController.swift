a5b8d2401e2038a3923d33c65c80b6dd22f8b4b5
CleAyz <clementayz@gmail.com>
2025-11-24 18:25:33 +0800

    Add text field delegate

--- UIKitTutorial/TextFields/TextFieldViewController.swift
+++ UIKitTutorial/TextFields/TextFieldViewController.swift
@@ -9,18 +9,22 @@ import UIKit
 
 class TextFieldViewController: UIViewController {
     
-    private var textField: UITextField = {
+    private lazy var textField: UITextField = {
         let tf = UITextField()
-        tf.placeholder = "Search..."
+        tf.placeholder = "Search...."
         tf.borderStyle = .roundedRect
         tf.keyboardType = .emailAddress
         tf.font = UIFont.systemFont(ofSize: 14)
         tf.returnKeyType = .search
+        tf.delegate = self
         return tf
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textField.addTarget(self, action: #selector(onEditingChanged), for: .editingChanged
+        )
         view.backgroundColor = .white
         
         view.addSubview(textField)
@@ -30,6 +34,26 @@ class TextFieldViewController: UIViewController {
         textField.widthAnchor.constraint(equalToConstant: view.frame.width - 64).isActive = true
     }
     
+    @objc func onEditingChanged(_ sender: UITextField) {
+        print("DEBUG: Text field text is \(sender.text)")
+    }
+    
+}
+
+extension TextFieldViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("DEBUG: Return key tapped...")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("DEBUG: Did begin editing...")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("DEBUG: Did end editing...")
+    }
 }
 
 #Preview {
